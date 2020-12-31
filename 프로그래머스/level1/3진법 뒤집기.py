6ca393b135bfeb4d83732e626bec740da38857a3
yunakim2 <98_02@naver.com>
2021-01-01 02:13:37 +0900

    프로그래머스 Level1 - 3진법 뒤집기

--- 프로그래머스/level1/3진법 뒤집기.py
+++ 프로그래머스/level1/3진법 뒤집기.py
@@ -1,5 +1,14 @@
 def solution(n):
-    answer = ''
-    while n<0 :
-        n
+    temp = []
+    while n >= 3 :
+        temp.append(str(n%3))
+        n = n//3
+
+    temp.append(str(n))
+    nums = ''.join(temp)
+    answer = int(nums,3)
     return answer
+
+
+
+print(solution(125))
