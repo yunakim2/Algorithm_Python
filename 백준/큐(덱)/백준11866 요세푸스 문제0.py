2421fdcdb59da380e44e4dd0e17dcfe627fbd4d7
yunakim2 <98_02@naver.com>
2021-01-06 19:44:04 +0900

    백준요세푸스 문제 0 해결

--- 백준/큐(덱)/백준11866 요세푸스 문제0.py
+++ 백준/큐(덱)/백준11866 요세푸스 문제0.py
@@ -2,16 +2,14 @@ n, k = map(int, input().split())
 q = [i for i in range(1,n+1)]
 print("<",end='')
 i = 0
-while len(q) > 1:
-    if len(q) >= i+k-1:
-        i = i+k-1
-        if i<0:
-            i +=1
-    else:
-        i = ((i+k) % len(q)) -1
-        if i < 0:
-            i+=k
 
+while len(q) > 1:
+    i = i+k
+    if i > len(q):
+        i = i % len(q)
+        if i == 0 :
+            i = i+ len(q)
+    i = i-1
     print(str(q.pop(i)), end=", ")
 
 print("{}>".format(str(q.pop())))
