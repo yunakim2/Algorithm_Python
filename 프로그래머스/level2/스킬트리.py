647fd372e9c67db15876ae43d9dbb4a95006026c
yunakim2 <98_02@naver.com>
2021-01-08 00:58:48 +0900

    스킬트리 : for - else 문으로 바꾸기

--- 프로그래머스/level2/스킬트리.py
+++ 프로그래머스/level2/스킬트리.py
@@ -1,22 +1,16 @@
 def solution(skill, skill_trees):
-    answer = 0
     skill_list = list(skill)
+    answer = 0
     for skill_item in skill_trees:
-        lists = list(reversed(skill_item))
+        lists = list(skill_item)
         correct = 0
-        flag = False
-        while len(lists) >= 0:
-            if len(lists) == 0:
-                flag = True
-                break
-            item = lists.pop()
+        for item in lists:
             if item in skill_list:
                 if skill_list[correct] == item:
                     correct += 1
                 else:
-                    flag = False
                     break
-        if flag:
+        else:
             answer += 1
 
     return answer
