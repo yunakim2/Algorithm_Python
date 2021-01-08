35fcc4ea7a6fdf76d039b7fd51ecbae263ce9995
yunakim2 <98_02@naver.com>
2021-01-08 17:58:13 +0900

    프로그래머스 Lv 2 - 기능 개발 구현

---
+++ 프로그래머스/level2/기능개발.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+
+def solution(progresses, speeds):
+    answer = []
+    queue = []
+    for progress, speed in zip(progresses, speeds):
+        queue.append((100 - progress) // speed)
+
+    i = 0
+    for j in range(1, len(queue)):
+        if queue[j] < queue[i]:
+            queue[j] = queue[i]
+        i += 1
+
+    count = Counter(queue).items()
+    for item in count:
+        answer.append(item[1])
+
+    return answer
+
+
+print(solution(	[93, 30, 55],  [1, 30, 5]))
