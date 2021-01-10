dc6d99b10157d5cabbd29896ca8bb0b8a2cbc272
yunakim2 <98_02@naver.com>
2021-01-10 23:46:53 +0900

    프로그래머스 Lv3 게임 아이템 시도 중

---
+++ 프로그래머스/level3/게임 아이템.py
@@ -0,0 +1,18 @@
+from collections import Counter
+def solution(healths, items):
+    answer = []
+    power = 0
+    for health in healths:
+        for i, item in enumerate(items):
+            if health - item[1] >= 100:
+                tmp_power = item[0] + power
+                if tmp_power > power:
+                    power = tmp_power
+                    answer.append(i + 1)
+
+    return sorted(Counter(answer).keys())
+
+
+if __name__ == '__main__':
+    print(solution([200, 120, 150], [[30, 100], [500, 30], [100, 400]]))
+    print(solution([300, 200, 500], [[1000, 600], [400, 500], [300, 100]]))
