99c9b6fb25488a3cc8aa81a62a428ecefb7cfc03
yunakim2 <98_02@naver.com>
2021-01-24 22:37:12 +0900

    주사위 게임 코드 수정

--- 프로그래머스/level2/주사위 게임.py
+++ 프로그래머스/level2/주사위 게임.py
@@ -3,12 +3,12 @@ from itertools import product
 
 def solution(monster, S1, S2, S3):
     answer = 0
-    range_S1 = list(idx for idx in range(1, S1 + 1))
-    range_S2 = list(idx for idx in range(1, S2 + 1))
-    range_S3 = list(idx for idx in range(1, S3 + 1))
+    range_S1 = list(range(1, S1 + 1))
+    range_S2 = list(range(1, S2 + 1))
+    range_S3 = list(range(1, S3 + 1))
 
-    for i, j, k in list(product(range_S1, range_S2, range_S3)):
-        move = i + j + k + 1
+    for prod in product(range_S1, range_S2, range_S3):
+        move = sum(prod) + 1
         if move not in monster:
             answer += 1
 
