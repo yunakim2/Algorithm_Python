78f48dbf53320f666c837b142fd0a958a661499d
yunakim2 <98_02@naver.com>
2021-01-26 16:36:38 +0900

    프로그래머스 Lv4 - 버스여행 - 플로이드웨셜 알고리즘

--- 프로그래머스/level4/버스 여행.py
+++ 프로그래머스/level4/버스 여행.py
@@ -1,23 +1,12 @@
-from collections import deque
-
-
 def solution(n, signs):
-    check_sign = [[False for _ in range(n)] for _ in range(n)]
-
-    def dfs(i, n, row):
-        if i == n:
-            return
-        if check_sign[row]:
-            return
-        check_sign[row] = True
-        for col in range(n):
-            if signs[row][col] == 1 and not check_sign[col]:
-                dfs(i+1, n, col)
-
-    dfs(0, n, 0)
-    print(signs)
-    return
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if signs[i][k] != 0 and signs[k][j] != 0:
+                    signs[i][j] = 1
+    return signs
 
 
 if __name__ == "__main__":
     print(solution(3, [[0, 1, 0], [0, 0, 1], [1, 0, 0]]))
+    print(solution(3,[[0,0,1],[0,0,1],[0,1,0]]))
