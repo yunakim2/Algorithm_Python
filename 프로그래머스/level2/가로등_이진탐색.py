ce6ea4aad5acd9c258a23edb75b231cb025e1352
yunakim2 <98_02@naver.com>
2021-01-26 16:16:50 +0900

    프로그래머스 Lv2 - 가로등 - 이진탐색

--- 프로그래머스/level2/가로등_이진탐색.py
+++ 프로그래머스/level2/가로등_이진탐색.py
@@ -1,11 +1,25 @@
 def solution(l, v):
-    answer = 0
+    lower = 1
+    upper = l + 1
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+    def is_possible(d):
+        if 0 < v[0] - d or v[-1] + d < l:
+            return False
+        for i in range(1,len(v)):
+            if v[i-1] + d < v[i] - d:
+                return False
+        return True
 
-    return answer
+    v.sort()
+    while lower <= upper:
+        mid = (lower + upper) // 2
+        if is_possible(mid):
+            upper = mid - 1
+        else:
+            lower = mid + 1
+    return upper + 1
 
 
 if __name__ == "__main__":
-    print(solution(15,[15,5,3,7,9,14,0]	))
+    print(solution(15, [15, 5, 3, 7, 9, 14, 0]))
+    print(solution(5,[2,5]))
