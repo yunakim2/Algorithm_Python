b86cbe5f602cb9b17b5ba8c021a419264f0f3828
yunakim2 <98_02@naver.com>
2021-01-26 16:23:57 +0900

    프로그래머스 Lv2 - 가로등 - Greedy

--- 프로그래머스/level2/가로등_그리디.py
+++ 프로그래머스/level2/가로등_그리디.py
@@ -1,34 +1,14 @@
+import math
+
+
 def solution(l, v):
     v.sort()
+    max_d = max(v[0], l - v[-1])
 
-    def check(d):
-        check_bright = [False for _ in range(l + 1)]
-        for item in v:
-            min_v = item - d
-            max_v = item + d
-            if min_v < 0 :
-                min_v = 0
-            if max_v > l:
-                max_v = l
-            f
-        return check_bright
-
-    d = set()
-    for idx, item in enumerate(v):
-        if 1 <= idx < len(v):
-            tmp_item = item - v[idx-1]
-            if tmp_item > 0:
-                d.add(tmp_item)
-        elif idx == 0:
-            tmp_item = item - 0
-            if tmp_item > 0:
-                d.add(tmp_item)
-    while d:
-        check_d = d.pop()
-        check_b = check(check_d)
-        if False not in check_b:
-            return check_d
-
+    for i in range(1, len(v)):
+        d = math.ceil((v[i] - v[i - 1]) / 2)
+        max_d = max(max_d, d)
+    return max_d
 
 
 if __name__ == "__main__":
