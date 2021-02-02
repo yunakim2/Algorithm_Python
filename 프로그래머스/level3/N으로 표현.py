5d3f6d86414e8f20764cc59e0082ac1cee3947dc
yunakim2 <98_02@naver.com>
2021-02-03 00:25:23 +0900

    프로그래머스 N으로 표현

--- 프로그래머스/level3/N으로 표현.py
+++ 프로그래머스/level3/N으로 표현.py
@@ -1,7 +1,25 @@
 def solution(N, number):
-    answer = 0
-    return answer
+    def blend(number1, number2):
+        blended = set()
+        for num1 in number1:
+            for num2 in number2:
+                blended.add(num1 + num2)
+                blended.add(num1 - num2)
+                blended.add(num1 * num2)
+                if num2 != 0:
+                    blended.add(num1 // num2)
+        return blended
+
+    dp = [set() for _ in range(8 + 1)]
+    for count in range(1, 8 + 1):
+        dp[count].add(int(str(N) * count))
+        for i in range(1, count):
+            dp[count] |= blend(dp[i], dp[count - i])
+        print(dp[count])
+        if number in dp[count]:
+            return count
+    return -1
 
 
 if __name__ == "__main__":
-    print(solution(5,12))
+    print(solution(5, 12))
--- 프로그래머스/level3/가장 긴 팰린드롬.py
+++ 프로그래머스/level3/가장 긴 팰린드롬.py
@@ -1,6 +1,22 @@
 def solution(s):
-    
+    def is_palindrome(word):
+        if len(word) <= 1:
+            return True
+        if word[0] == word[-1]:
+            return is_palindrome(word[1:-1])
+        else:
+            return False
+
+    for idx in range(len(s), 0, -1):
+        for start in range(0, len(s)):
+            word = s[start:start + idx]
+            if is_palindrome(word):
+                return len(word)
+            if start + idx >= len(s):
+                break
+    return 0
 
 
 if __name__ == "__main__":
     print(solution("abcdcba"))
+    print(solution("abacde"))
