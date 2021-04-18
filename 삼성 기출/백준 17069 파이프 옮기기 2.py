10030d716e7fc5d4388881c299b8f21f34f736ca
yunakim2 <98_02@naver.com>
2021-04-18 17:27:45 +0900

    백준 파이프 옮기기 2

--- 삼성 기출/백준 17069 파이프 옮기기 2.py
+++ 삼성 기출/백준 17069 파이프 옮기기 2.py
@@ -1,35 +1,32 @@
-def check(x, y):
-    if 0 <= x < n and 0 <= y < n and maps[x][y] != 1:
-        return True
-    return False
+
 
 
 def dfs(r1, c1, mode):
-    global ans
     if r1 == n - 1 and c1 == n - 1:
         return 1
     ans = cnt[r1][c1][mode]
     if ans != -1:
         return ans
+
     ans = 0
     if mode == H:
-        if check(r1, c1 + 1):
+        if c1 + 1 < n and maps[r1][c1 + 1] == 0:
             ans += dfs(r1, c1 + 1, H)
-        if check(r1 + 1, c1 + 1) and check(r1, c1 + 1) and check(r1 + 1, c1):
+        if r1 + 1 < n and c1 + 1 < n and maps[r1 + 1][c1 + 1] == 0 and maps[r1 + 1][c1] == 0 and maps[r1][c1 + 1] == 0:
             ans += dfs(r1 + 1, c1 + 1, D)
 
     elif mode == V:
-        if check(r1 + 1, c1):
+        if r1+1 <n and maps[r1+1][c1] == 0:
             ans += dfs(r1 + 1, c1, V)
-        if check(r1 + 1, c1 + 1) and check(r1, c1 + 1) and check(r1 + 1, c1):
+        if  r1 + 1 < n and c1 + 1 < n and maps[r1 + 1][c1 + 1] == 0 and maps[r1 + 1][c1] == 0 and maps[r1][c1 + 1] == 0:
             ans += dfs(r1 + 1, c1 + 1, D)
 
     else:
-        if check(r1 + 1, c1):  # 세로
+        if r1 + 1 < n and maps[r1 + 1][c1] == 0:
             ans += dfs(r1 + 1, c1, V)
-        if check(r1, c1 + 1):  # 가로
+        if c1 + 1 < n and maps[r1][c1 + 1] == 0:
             ans += dfs(r1, c1 + 1, H)
-        if check(r1 + 1, c1 + 1) and check(r1, c1 + 1) and check(r1 + 1, c1):
+        if  r1 + 1 < n and c1 + 1 < n and maps[r1 + 1][c1 + 1] == 0 and maps[r1 + 1][c1] == 0 and maps[r1][c1 + 1] == 0:
             ans += dfs(r1 + 1, c1 + 1, D)
     cnt[r1][c1][mode] = ans
     return ans
@@ -39,11 +36,9 @@ if __name__ == '__main__':
     n = int(input())
 
     maps = [list(map(int, input().split())) for _ in range(n)]
-    cnt = [[[-1] * 3 for _ in range(n)] for _ in range(n)]
-    ans = 0
+    cnt = [[[-1] * 3 for j in range(n)] for i in range(n)]
     H = 0
     D = 1
     V = 2
-    if maps[n - 1][n - 1] != 1:
-        print(dfs(0, 1, H))
+    print(dfs(0, 1, H))
     '''H - 가로, V-세로, D - 대각선'''
